d5ab6deda96e8cf0892ce46ca7311bc41440df69
nklapste <nklapste@ualberta.ca>
2017-11-20 08:46:32 -0700

    adding part 2 soln

--- src/part2/part2.java
+++ src/part2/part2.java
@@ -18,43 +18,45 @@ public class part2 {
 }
 
 //TODO
+/*
+ * This is preferred as it is straightforward and easy to read. At each step we know that we are multiplying and due to
+ * the method being extremely simple and easy to read these qualities are transferred to the code.
+ */
 public class Part2_inline_temp {
     public int funny(int a, int b) {
+        if (a * b > 100) {
 
-        int temp = a * b;
-
-        if (temp > 100) {
-
-            return temp * 0.95;
+            return a * b * 0.95;
 
         } else {
 
-            return temp * 0.25;
+            return a * b * 0.25;
 
         }
     }
 }
 
 //TODO
+/*
+ * This is not preferred as it is redundant for such a simple method. For a more advanced or lengthy method
+ * this can be useful, but, for a small and simple function of multiplying two numbers this is overkill.
+ *
+ */
 public class Part2_extract_method_blah {
     public int funny(int a, int b) {
+        if (getTemp(a, b) > 100) {
 
-        int temp = a * b;
-
-        if (temp > 100) {
-
-            return temp * 0.95;
+            return getTemp(a, b) * 0.95;
 
         } else {
 
-            return temp * 0.25;
+            return getTemp(a, b) * 0.25;
 
         }
+
+    }
+
+    int getTemp(int a, int b){
+        return a * b;
     }
 }
-
-//TODO discuss which refactor method is better
-/*
- *
- *
- */
