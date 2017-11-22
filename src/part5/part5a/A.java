454bc8d41ce753d7c96032224302729c8b6ce274
nklapste <nklapste@ualberta.ca>
2017-11-22 11:09:40 -0700

    adding partial answer for part5

--- src/part5/part5a/A.java
+++ src/part5/part5a/A.java
@@ -3,6 +3,7 @@ package part5.part5a;
 
 public class A {
 
+    // This k function will be used in part A, thus, output will be 10
     public int k(long i) {
 
         return 10;
--- src/part5/part5b/A.java
+++ src/part5/part5b/A.java
@@ -8,6 +8,9 @@ public class A {
 
     }
 
+    // This k function is used in B (due to overloading and by
+    // having its input parameter type (int) being closer type to
+    // int than long in the other k function) thus, output will be 20
     public int k(int i) {
 
         return 20;
