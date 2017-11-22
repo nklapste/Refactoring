62c4003f99fddab92dcf0c309fbfc014d2a56c9d
nklapste <nklapste@ualberta.ca>
2017-11-22 11:15:21 -0700

    added comment on part5a

--- src/part5/part5a/B.java
+++ src/part5/part5a/B.java
@@ -9,7 +9,9 @@ public class B extends A {
     }
 
     public static void main(String[] args) {
-
+        // A().k(2) call makes local k function unused
+        // as we specify for superclass's k function
+        // specifically
         System.out.println(new A().k(2));
 
     }
